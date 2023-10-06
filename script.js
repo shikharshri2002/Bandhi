f6129c0544557e0edc1c75e7d15fc0237e10bbf7
shikharshri2002 <143315977+shikharshri2002@users.noreply.github.com>
2023-10-06 23:26:28 +0530

    added javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.video-container video').forEach(vid =>{
+vid.onclick = () =>{
+    document.querySelector('.popup-video').style.display ='block';
+    document.querySelector('.popup-video video').src=vid.getAttribute('src');
+}
+ });
+
+ document.querySelector('.popup-video span').onclick =() =>{
+    document.querySelector('.popup-video').style.display ='none';
+    document.querySelector('.popup-video video').pause(); 
+ }
+
+ 
